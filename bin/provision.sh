1c506065abcd0fbb1b5f8a77c9f41a0d78bd7cd1
Igor Malinovskiy <u.glide@gmail.com>
2015-09-04 11:14:13 +0300

    Update provision.sh

--- bin/provision.sh
+++ bin/provision.sh
@@ -1,10 +1,24 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
+cd /vagrant
+
+# update packages
 sudo apt-get update
 sudo apt-get build-dep python -y
 sudo apt-get install git python-pip -y
+sudo apt-get install postgresql postgresql-contrib -y
+sudo pip install setuptools --upgrade
+sudo apt-get purge cloud-init -y
 
+#prepare env
+sudo chown -R vagrant /opt
+sudo rm -fR /vagrant/stack
+sudo rm -fR /vagrant/devstack
+
+#get devstack
 git clone https://git.openstack.org/openstack-dev/devstack
-chown -R vagrant devstack
+cp local.conf.dev devstack/local.conf
 
-
+sudo chown -R vagrant devstack/
+sudo chown -R vagrant /opt/stack/
+cd devstack
